410d07da8c3ffab1802d8a598ba063dc38eaae52
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2023-12-29 22:52:06 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
 
-namespace WebApplication1.Pages
+namespace test.Pages.Clients
 {
     public class IndexModel : PageModel
     {
+       
+        public List<ClientInfo> ListClients { get; set; }
+
+        List<ClientInfo> listClients= new List<ClientInfo>();
         public void OnGet()
         {
+            try
+            {
+                string connectionString = "Data Source=Victusx15\\DATABASE_SERVER;Initial Catalog=test;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM clients";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.ID = reader.GetInt32(0).ToString();
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
+    }
+
+    public class ClientInfo
+    {
+        public string ID;
+        public string name;
+        public string email;
+        public string phone;
+        public string address;
+        public string created_at;
     }
 }
