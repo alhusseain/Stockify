673c7e34663d17fa102d619cd189f85f565e6306
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2023-12-29 22:55:31 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,73 +1,87 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Data.SqlClient;
-using System.Data;
-using System.Linq.Expressions;
+@page
+@model WebApplication1.Pages.IndexModel
+@{
+    Layout = "";
+    ViewData["Title"] = "Choose Who You Are";
+}
 
-namespace WebApplication1.Pages
-{
-    public class EmployeesModel : PageModel
-    {
-        public List<EmployeeInfo> Employee = new List<EmployeeInfo>();
-
-
-        public void OnGet( )
-        {
-            try
-            {
-                string connecectionstring = "Data Source=Victusx15\\DATABASE_SERVER;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-
-                using (SqlConnection connection = new SqlConnection(connecectionstring))
-                {
-                    connection.Open();
-                    string sqlQuery = "SELECT * FROM Employee";
-
-                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                    {
-
-
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                EmployeeInfo employee = new EmployeeInfo();
-
-                                employee.Fname =  reader.GetString(0);
-                                employee.EmployeeID = reader.GetInt32(1);
-                                employee.RoleName = reader.GetString(2);
-                                employee.Phonenumber = reader.GetString(8);
-                                employee.Branch_ID = reader.GetInt32(9);
-
-                                Employee.Add(employee);
-
-
-                            }
-                        }
-
-                    }
-                }
-
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine("Exception" + ex.ToString());
-            }
-            finally
-            {
-
-            }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stockify</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
         }
 
-       
-    }
+        .container {
+            max-width: 400px;
+            margin: auto;
+            margin-top: 50px;
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
 
-    public class EmployeeInfo
-    {
-        public string Fname;
-        public int EmployeeID;
-        public string RoleName;
-        public string Phonenumber;
-        public int Branch_ID;
-        
-    }
-}
+        h2 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .header {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .btn-choose {
+            width: 100%;
+            cursor: pointer;
+            margin-bottom: 10px;
+        }
+
+            .btn-choose:hover {
+                background-color: #007bff;
+                color: #ffffff;
+            }
+    </style>
+</head>
+<body>
+    <div class="container">
+
+        <div class="logo">
+            <img src="~/static/logo.png" alt="Logo" height="125" width="125">
+        </div>
+
+        <div class="header">
+            <h2>Welcome to Stockify!</h2>
+        </div>
+
+        <div class="header">
+            <h3>Choose Your Role</h3>
+        </div>
+
+        <a onclick="setRoleAndRedirect('Manager')" class="btn btn-choose btn-manager">Manager</a>
+        <a onclick="setRoleAndRedirect('Cashier')" class="btn btn-choose btn-cashier">Cashier</a>
+        <a onclick="setRoleAndRedirect('Maintainer')" class="btn btn-choose btn-maintainer">Maintainer</a>
+        <a onclick="setRoleAndRedirect('Transporter')" class="btn btn-choose btn-transporter">Transporter</a>
+
+        <script>
+            function setRoleAndRedirect(role) {
+                document.cookie = "userRole=" + role;
+                window.location.href = "/signin"; // Redirect to the signin page
+            }
+        </script>
+
+    </div>
+</body>
+</html>
