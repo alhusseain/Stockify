d240b0b91181dcc23b76a7ededa54375fbaacb89
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2023-12-29 22:54:41 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,62 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
 
-namespace test.Pages.Clients
+namespace WebApplication1.Pages
 {
-    public class IndexModel : PageModel
+    public class EmployeesModel : PageModel
     {
-       
-        public List<ClientInfo> ListClients { get; set; }
+        public List<EmployeeInfo> Employee = new List<EmployeeInfo>();
 
-        List<ClientInfo> listClients= new List<ClientInfo>();
-        public void OnGet()
+
+        public void OnGet( )
         {
             try
             {
-                string connectionString = "Data Source=Victusx15\\DATABASE_SERVER;Initial Catalog=test;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+                string connecectionstring = "Data Source=Victusx15\\DATABASE_SERVER;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(connecectionstring))
                 {
                     connection.Open();
-                    string sql = "SELECT * FROM clients";
+                    string sqlQuery = "SELECT * FROM Employee";
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
                     {
+
+
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                ClientInfo clientInfo = new ClientInfo();
-                                clientInfo.ID = reader.GetInt32(0).ToString();
-                                clientInfo.name = reader.GetString(1);
-                                clientInfo.email = reader.GetString(2);
-                                clientInfo.phone = reader.GetString(3);
-                                clientInfo.address = reader.GetString(4);
-                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+                                EmployeeInfo employee = new EmployeeInfo();
 
-                                listClients.Add(clientInfo);
+                                employee.Fname =  reader.GetString(0);
+                                employee.EmployeeID = reader.GetInt32(1);
+                                employee.RoleName = reader.GetString(2);
+                                employee.Phonenumber = reader.GetString(8);
+                                employee.Branch_ID = reader.GetInt32(9);
+
+                                Employee.Add(employee);
+
+
                             }
                         }
+
                     }
                 }
+
             }
-            catch (Exception ex)
+            catch (Exception ex) 
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Exception" + ex.ToString());
+            }
+            finally
+            {
+
             }
         }
+
+       
     }
 
-    public class ClientInfo
+    public class EmployeeInfo
     {
-        public string ID;
-        public string name;
-        public string email;
-        public string phone;
-        public string address;
-        public string created_at;
+        public string Fname;
+        public int EmployeeID;
+        public string RoleName;
+        public string Phonenumber;
+        public int Branch_ID;
+        
     }
 }
