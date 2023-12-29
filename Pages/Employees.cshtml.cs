c3237743ecff9c993d68e83b1e32611670f49df4
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2023-12-29 22:57:42 +0200

    Update Employees.cshtml.cs

--- Pages/Employees.cshtml.cs
+++ Pages/Employees.cshtml.cs
@@ -1,12 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Linq.Expressions;
 
 namespace WebApplication1.Pages
 {
     public class EmployeesModel : PageModel
     {
-        public void OnGet()
+        public List<EmployeeInfo> Employee = new List<EmployeeInfo>();
+
+
+        public void OnGet( )
         {
+            try
+            {
+                string connecectionstring = "Data Source=Victusx15\\DATABASE_SERVER;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+                using (SqlConnection connection = new SqlConnection(connecectionstring))
+                {
+                    connection.Open();
+                    string sqlQuery = "SELECT * FROM Employee";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                EmployeeInfo employee = new EmployeeInfo();
+
+                                employee.Fname =  reader.GetString(0);
+                                employee.EmployeeID = reader.GetInt32(1);
+                                employee.RoleName = reader.GetString(2);
+                                employee.Phonenumber = reader.GetString(8);
+                                employee.Branch_ID = reader.GetInt32(9);
+
+                                Employee.Add(employee);
+
+
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Exception" + ex.ToString());
+            }
+            finally
+            {
+
+            }
         }
+
+       
+    }
+
+    public class EmployeeInfo
+    {
+        public string Fname;
+        public int EmployeeID;
+        public string RoleName;
+        public string Phonenumber;
+        public int Branch_ID;
+        
     }
 }
