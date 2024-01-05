f80b1b739ba987b374eec4445f35bd2968edf606
Kareemaadel <kareem.adel20047@gmail.com>
2024-01-05 18:10:43 +0200

    yarab

--- Pages/Employees.cshtml.cs
+++ Pages/Employees.cshtml.cs
@@ -1,74 +1,94 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 using System.Data;
-using System.Linq.Expressions;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.SignalR;
+using System.Reflection.Metadata;
 
 namespace WebApplication1.Pages
 {
+
     public class EmployeesModel : PageModel
     {
-        public List<EmployeeInfo> Employee = new List<EmployeeInfo>();
+        private readonly ILogger<EmployeesModel> _logger;
+        public DB db { get; set; } = new DB();
+        public DataTable EmployeeDataTable { get; set; }
+        public Employee employee { get; set; }
+        [BindProperty]
+        public string employeeId { get; set; }
+        [BindProperty]
+        public string name { get; set; }
+        [BindProperty]
+        public string worksAs { get; set; }
+        [BindProperty]
+        public string phoneNumber { get; set; }
+        [BindProperty]
+        public string branch { get; set; }
 
-
-        public void OnGet( )
+        public EmployeesModel(ILogger<EmployeesModel> logger, DB db)
         {
+            _logger = logger;
+            this.db = db;
+        }
+
+        public void OnGet()
+        {
+            EmployeeDataTable = db.ReadTable("Employee");
+        }
+
+        public IActionResult OnPostDelete(string employeeId)
+        {
+            // Your delete logic
+            return new JsonResult(new { success = true });
+        }
+
+        public IActionResult OnPostUpdate(Employee emp)
+        {
+            // Your update logic
+            return new JsonResult(new { success = true });
+        }
+
+
+        public void OnPost()
+        {
+            // employee.EmployeeID=employeeId;
+            // employee.Fname=name;
+            // employee.RoleName=worksAs;
+            // employee.PhoneNumber=phoneNumber;
+            // employee.Branch_ID=branch;
+            // db.AddEmployee(employee);
+            // EmployeeDataTable = db.ReadTable("Employee");
+            string connection = "Server=DESKTOP-9IHIA03;Database=StockifyUpdated;Integrated Security=True;Encrypt=False;";
+            SqlConnection con = new SqlConnection(connection);
+            string query = "insert into Employee(Fname, EmployeeID, RoleName, PhoneNumber, Branch_ID) values(@Fname, @EmployeeID,@RoleName,@PhoneNumber,@Branch_ID)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Fname", name);
+            cmd.Parameters.AddWithValue("@EmployeeID", int.Parse(employeeId));
+            cmd.Parameters.AddWithValue("@RoleName", worksAs);
+            cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+            cmd.Parameters.AddWithValue("@Branch_ID", int.Parse(branch));
+            Console.Write(query);
+            string res = "";    // this is for storing error messages (if any) and returning them from the function 
+
             try
             {
-                string connecectionstring = "Server=KAREEM;Database=StockifyUpdated;Integrated Security=True;Encrypt=False;";
-
-                using (SqlConnection connection = new SqlConnection(connecectionstring))
-                {
-                    connection.Open();
-                    string sqlQuery = "SELECT * FROM Employee";
-
-                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                    {
-
-
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                EmployeeInfo employee = new EmployeeInfo();
-
-                                employee.Fname =  reader.GetString(0);
-                                employee.EmployeeID = reader.GetInt32(1);
-                                employee.RoleName = reader.GetString(2);
-                                employee.Phonenumber = reader.GetString(8);
-                                employee.Branch_ID = reader.GetInt32(9);
-
-                                Employee.Add(employee);
-
-
-                            }
-                        }
-
-                    }
-                }
-
+                con.Open();
+                res = cmd.ExecuteNonQuery().ToString();
+                //return RedirectToPage("Employees");
+                //db.AddEmployee(employee);
+                //EmployeeDataTable = db.ReadTable("Employee");
             }
-            catch (Exception ex) 
+            catch (SqlException err)
             {
-                Console.WriteLine("Exception" + ex.ToString());
+                res = err.Message;
             }
             finally
             {
-
+                con.Close();
             }
+            //return Page();
+
         }
-
-       
-    }
-
-    public class EmployeeInfo
-    {
-        public string Fname;
-        public int EmployeeID;
-        public string RoleName;
-        public string Phonenumber;
-        public int Branch_ID;
-        
     }
 }
-
